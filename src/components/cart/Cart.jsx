4b5023fc5f2ff0b012218b48739caf352fde405a
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-26 10:35:22 +0500

    Add onCheckout

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -5,11 +5,16 @@ import { Button } from "../";
 import { priceOptions } from "../../constants/constants";
 import { totalPrice } from "../../helpers/totalPrice";
 
-const Cart = ({ cartItems }) => {
+const Cart = ({ cartItems, onCheckout }) => {
   return (
     <div className="cart">
       <p className="">Umumiy narx: {totalPrice(cartItems).toLocaleString("en-US", priceOptions)}</p>
-      <Button title={`${cartItems.length ? "Buyurtma berish" : "to'lov"}`} type="checkout" disabled={!cartItems.length} />
+      <Button
+        title={`${cartItems.length ? "Buyurtma berish" : "to'lov"}`}
+        type="checkout"
+        disabled={!cartItems.length}
+        click={onCheckout}
+      />
     </div>
   );
 };

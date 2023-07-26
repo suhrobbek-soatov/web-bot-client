bcccc90e3a8729836f332a57f0b2fd4f82d785bd
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-26 17:58:24 +0500

    fix bem class rules

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -12,7 +12,7 @@ const Cart = ({ cartItems, onCheckout }) => {
 
   return (
     <div className="cart">
-      <p className="">Umumiy narx: {totalPrice(cartItems).toLocaleString("en-US", priceOptions)}</p>
+      <p className="cart__text">Umumiy narx: {totalPrice(cartItems).toLocaleString("en-US", priceOptions)}</p>
       <Button
         title={`${cartItems.length ? "Buyurtma berish" : "to'lov"}`}
         type="checkout"

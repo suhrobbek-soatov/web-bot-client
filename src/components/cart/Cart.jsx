710a71434543bae5d04b5d64cd4a6e85755c5eb7
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-26 10:46:57 +0500

    Add hide telegram.MainButton hide

--- src/components/cart/Cart.jsx
+++ src/components/cart/Cart.jsx
@@ -5,7 +5,11 @@ import { Button } from "../";
 import { priceOptions } from "../../constants/constants";
 import { totalPrice } from "../../helpers/totalPrice";
 
+const telegram = window.Telegram.WebApp;
+
 const Cart = ({ cartItems, onCheckout }) => {
+  if (!cartItems.length) telegram.MainButton.hide();
+
   return (
     <div className="cart">
       <p className="">Umumiy narx: {totalPrice(cartItems).toLocaleString("en-US", priceOptions)}</p>

70433515c98c6d8cb7360fd1b555011e7246674a
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-26 09:49:41 +0500

    Add cart and cart's add and remove helpers

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,34 @@
+import { useState } from "react";
+import { Card, Cart } from "./components";
 import courses from "./data/db.json";
+
 const App = () => {
-  return <></>;
+  const [cartItems, setCartItems] = useState([]);
+
+  const onAddItem = item => {
+    const existItem = cartItems.find(c => c.id === item.id);
+
+    if (existItem) {
+      const newData = cartItems.map(c => (c.id === item.id ? { ...existItem, quantity: existItem.quantity + 1 } : c));
+      setCartItems(newData);
+    } else {
+      const newData = [...cartItems, { ...item, quantity: 1 }];
+      setCartItems(newData);
+    }
+  };
+
+  const onRemoveItem = item => {
+    const existItem = cartItems.find(c => c.id === item.id);
+
+    if (existItem.quantity > 1) {
+      const newData = cartItems.map(c => (c.id === item.id ? { ...existItem, quantity: existItem.quantity - 1 } : c));
+      setCartItems(newData);
+    } else {
+      const newData = cartItems.filter(c => c.id !== item.id);
+      setCartItems(newData);
+    }
+  };
+
   return (
     <main className="main-content">
       <h1 className="heading">WebEdu Kurslar</h1>

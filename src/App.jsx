0af8e48e152ce146e50d01a058f7c0d4b750101c
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-26 09:49:18 +0500

    Map iteation courses

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,17 @@
+import courses from "./data/db.json";
 const App = () => {
   return <></>;
+  return (
+    <main className="main-content">
+      <h1 className="heading">WebEdu Kurslar</h1>
+      <Cart cartItems={cartItems} />
+      <div className="cards container">
+        {courses.map(course => (
+          <Card key={course.id} onAddItem={onAddItem} onRemoveItem={onRemoveItem} course={course} />
+        ))}
+      </div>
+    </main>
+  );
 };
 
 export default App;

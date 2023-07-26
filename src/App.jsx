6c39099be6dfcae445a124c4fec88a802220deb0
svkhrobbeck <svkhrobbeck@gmail.com>
2023-07-26 15:11:21 +0500

    update

--- src/App.jsx
+++ src/App.jsx
@@ -44,7 +44,7 @@ const App = () => {
   const onSendData = useCallback(() => {
     const queryId = telegram.initDataUnSave?.query_id;
 
-    if (queryId) axios.post("https://sbk-web-bot-c7d8c058770d.herokuapp.com/", { courses: cartItems, queryId });
+    if (queryId) axios.post("https://sbk-web-bot-c7d8c058770d.herokuapp.com/web-bot", { courses: cartItems, queryId });
     else telegram.sendData(JSON.stringify(cartItems));
   }, [cartItems]);
 
